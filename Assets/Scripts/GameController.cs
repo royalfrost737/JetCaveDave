6d4c4b644996991e0fd9a9b2f15d9c1d987154c9
nellid <janelle.michaelis@utah.edu>
2015-11-24 18:35:49 -0800

    Alpha

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,7 +14,7 @@ public class GameController : MonoBehaviour
 	public AudioClip EnemyKill;
 	public AudioClip Powerup;
 
-	private Vector3 spawnPosition;
+	private Vector3 spawnPosition, spawnPosition1, spawnPosition2, spawnPosition3, spawnPosition4;
 
 	// The game controller subscribes to these events from other classes
 	void OnEnable()
@@ -46,27 +46,63 @@ public class GameController : MonoBehaviour
 
    void SpawnWaves(string direction, Vector3 navigation)
     {
-		for (int i = 0; i < hazardCount; i++) {
-			if (player.GetComponent<PlayerController> ().direction == "vertical") {
-				if (navigation.z < 0) {
+		for (int i = 0; i < hazardCount; i++) 
+		{
+			if (player.GetComponent<PlayerController> ().direction == "vertical") 
+			{
+				if (navigation.z < 0) 
+				{
 					spawnPosition = new Vector3 (player.transform.position.x, player.transform.position.y, player.transform.position.z + 3 + i);
+					spawnPosition1 = new Vector3 (player.transform.position.x+.4f, player.transform.position.y, player.transform.position.z + 3 + i);
+					spawnPosition2 = new Vector3 (player.transform.position.x-.4f, player.transform.position.y, player.transform.position.z + 3 + i);
+					spawnPosition3 = new Vector3 (player.transform.position.x+.8f, player.transform.position.y, player.transform.position.z + 3 + i);
+					spawnPosition4 = new Vector3 (player.transform.position.x-.8f, player.transform.position.y, player.transform.position.z + 3 + i);
 
 				}
-				if (navigation.z > 0) {
+				if (navigation.z > 0) 
+				{
 					spawnPosition = new Vector3 (player.transform.position.x, player.transform.position.y, player.transform.position.z - 3 - i);
+					spawnPosition1 = new Vector3 (player.transform.position.x+.4f, player.transform.position.y, player.transform.position.z - 3 - i);
+					spawnPosition2 = new Vector3 (player.transform.position.x-.4f, player.transform.position.y, player.transform.position.z - 3 - i);
+					spawnPosition3 = new Vector3 (player.transform.position.x+.8f, player.transform.position.y, player.transform.position.z - 3 - i);
+					spawnPosition4 = new Vector3 (player.transform.position.x-.8f, player.transform.position.y, player.transform.position.z - 3 - i);
 
 				}
 			}
-			if (player.GetComponent<PlayerController> ().direction == "horizontal") {
-				if (navigation.x < 0) {
+			if (player.GetComponent<PlayerController> ().direction == "horizontal") 
+			{
+				if (navigation.x < 0) 
+				{
 					spawnPosition = new Vector3 (player.transform.position.x + 3 + i, player.transform.position.y, player.transform.position.z);
+					spawnPosition1 = new Vector3 (player.transform.position.x + 3 + i, player.transform.position.y, player.transform.position.z+.4f);
+					spawnPosition2 = new Vector3 (player.transform.position.x + 3 + i, player.transform.position.y, player.transform.position.z-.4f);
+					spawnPosition3 = new Vector3 (player.transform.position.x + 3 + i, player.transform.position.y, player.transform.position.z+.8f);
+					spawnPosition4 = new Vector3 (player.transform.position.x + 3 + i, player.transform.position.y, player.transform.position.z-.8f);
+
 				}
-				if (navigation.x > 0) {
+				if (navigation.x > 0) 
+				{
 					spawnPosition = new Vector3 (player.transform.position.x - 3 - i, player.transform.position.y, player.transform.position.z);
+					spawnPosition1 = new Vector3 (player.transform.position.x - 3 - i, player.transform.position.y, player.transform.position.z+.4f);
+					spawnPosition2 = new Vector3 (player.transform.position.x - 3 - i, player.transform.position.y, player.transform.position.z-.4f);
+					spawnPosition3 = new Vector3 (player.transform.position.x + 3 + i, player.transform.position.y, player.transform.position.z+.8f);
+					spawnPosition4 = new Vector3 (player.transform.position.x + 3 + i, player.transform.position.y, player.transform.position.z-.8f);
+
 				}
 			}
 			Quaternion spawnRotation = Quaternion.identity;
 			Instantiate (Octo, spawnPosition, spawnRotation);
+			if (i > 9)
+			{
+				Instantiate (Octo, spawnPosition1, spawnRotation);
+				Instantiate (Octo, spawnPosition2, spawnRotation);
+			}
+			if (i > 19)
+			{
+				Instantiate (Octo, spawnPosition3, spawnRotation);
+				Instantiate (Octo, spawnPosition4, spawnRotation);
+			}
+
 		}
     }
 
